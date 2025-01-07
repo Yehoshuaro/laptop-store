419f35bf7c0dd9de6716db8574e7aa590bf1611c
Yerkhan <231132@astanait.edu.kz>
2025-01-07 21:58:15 +0500

    test.js file npm install axios

---
+++ test.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+async function testPost() {
+    const laptop = {
+        name: "Lenovo ThinkPad",
+        brand: "Lenovo",
+        price: 1200,
+        specifications: {
+            CPU: "Intel i7",
+            RAM: "16GB",
+            Storage: "512GB SSD"
+        },
+        category: "Business",
+        in_stock: true
+    };
+
+    try {
+        const response = await axios.post("http://127.0.0.1:3000/laptops", laptop);
+        console.log("Added laptop:", response.data);
+    }
+    catch (error) {
+        console.error("Error:", error.response ? error.response.data : error.message);
+    }
+}
+
+testPost();
